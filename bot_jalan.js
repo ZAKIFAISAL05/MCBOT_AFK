549f0f93a9e07b82c27a624163b6cfb0b32d0a9c
zakifaisal05 <narutoacmilan1@gmail.com>
2026-04-08 07:47:52 +0700

    Update bot_jalan.js

--- bot_jalan.js
+++ bot_jalan.js
@@ -2,12 +2,12 @@ const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
   host: 'Server_Partner.aternos.me',  // ← UBAH
-  port: 60725,                        // ← UBAH
+  port: 60725,                        // ← UBAH  
   username: 'Bandotrok',
   version: false
 });
 
-console.log('🤖 Bandotrok Keep-Alive v1.0');
+console.log('🤖 Bandotrok Keep-Alive');
 console.log('📡 ' + bot.options.host + ':' + bot.options.port);
 
 bot.on('spawn', () => {
@@ -15,28 +15,16 @@ bot.on('spawn', () => {
   bot.chat('/login 123456');
 });
 
-bot.on('message', (message) => {
-  console.log(message.toString());
+bot.on('message', (m) => {
+  console.log(m.toString());
 });
 
-bot.on('end', () => {
-  console.log('🔄 Restarting...');
-  setTimeout(() => {
-    require('child_process').spawn(process.argv[0], process.argv.slice(1), {
-      detached: true,
-      stdio: 'ignore'
-    }).unref();
-    process.exit();
-  }, 5000);
-});
-
-// Ping every 15 minutes
 setInterval(() => {
   if (bot.entity) {
     bot.chat('kek');
   }
-}, 15 * 60 * 1000);
+}, 900000); // 15 min
 
-process.on('uncaughtException', (err) => {
-  console.log('Error:', err.message);
+bot.on('end', () => {
+  console.log('🔄 Restart...');
 });
